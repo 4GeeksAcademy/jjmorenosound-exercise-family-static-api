da3df2d7e9ab9f85a128284a05d6c96443f70609
Jorge Moreno <jjmorenoaudio@gmail.com>
2024-12-14 16:18:35 +0000

    omw

--- src/app.py
+++ src/app.py
@@ -15,6 +15,15 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+
+new_member = jackson_family.add_member (  {
+             'name': 'John',
+            
+             'age': 33,
+             'lucky_numbers':'7, 13, 22'
+             })
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -31,12 +40,36 @@ def handle_hello():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
-        "family": members
+        "members": members
     }
 
 
     return jsonify(response_body), 200
+
+@app.route('/members', methods=['POST'])
+def handle_add_member():
+        member = request.get_json()
+
+        if member is None:
+            return 'Error body not found', 400
+        if member ['name'] is None:
+            return 'Error name is not found', 400
+        if member ['age'] is None:
+            return 'Error age is not found', 400
+        if member ['lucky_numbers'] is None:
+            return 'Error lucky numbers is not found', 400
+        
+        new_member = jackson_family.add_member(member)    
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def handle_delete_member():
+     position = <int:member_id>
+     if 0 <= position < len(members):
+        deleted_todo = members.pop(position)
+        print("This is the member to delete:", position)
+        return {"message": "Family member deleted successfully", "deleted_member": deleted_member}, 200
+    else:
+        return {"error": "Invalid position"}, 400
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,19 +13,40 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+        #   {
+        #     'name': 'John',
+        #     'last_name': last_name,
+        #     'age': 33,
+        #     'lucky_numbers': [7, 13, 22]
+        #   },
+        #   {
+        #     'name': 'Jane',
+        #     'last_name': last_name,
+        #     'age': 35,
+        #     'lucky_numbers': [10, 14, 3]
+        #   },
+        #   {
+        #     'name': 'Jimmy',
+        #     'last_name': last_name,
+        #     'age': 5,
+        #     'lucky_numbers': [1]
+        #   }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+      member ['id']= self._generateId();
+      member ['last_name'] = self.last_name;
+      self._members.append(member)
+      return member
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+       self._members.pop(id)
+       return []
 
     def get_member(self, id):
         # fill this method and update the return
@@ -34,3 +55,12 @@ class FamilyStructure:
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+    
+    def __repr__(self):
+        return "family lastname: {self.lastname}"
+
+    def serialize(self):
+        return {
+            'last_name': self.last_name,
+            'members': self._members
+        }
